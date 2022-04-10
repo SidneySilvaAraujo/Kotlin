a2fbc670001662a7796633bdc63b5c9aca5d6d61
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-04-10 07:34:02 -0300

    Criação do bitybank

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
-fun main() {
-    println("Bem vindo ao bytebank")
+fun main(args: Array<String>) {
+    var titular = "Sidney"
+    val numeroConta = 1234
+var saldo = 0.0
+    println("Bem vindo ao bytebank!")
+    println("Olá $titular sua conta é: $numeroConta")
+println("Saldo da conta: $saldo")
+
+print(saldo)
+
 }
+
