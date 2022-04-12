3a868901b9bc402ce0fb5039846aa2d19a163cde
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2022-04-12 15:45:35 -0300

    Testando condicionais

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,12 +1,19 @@
+
 fun main(args: Array<String>) {
     var titular = "Sidney"
     val numeroConta = 1234
-var saldo = 0.0
+    var saldo = -50.0
     println("Bem vindo ao bytebank!")
     println("Olá $titular sua conta é: $numeroConta")
-println("Saldo da conta: $saldo")
-
-print(saldo)
+    println("Saldo da conta: $saldo")
+    if (saldo > 0.0) {
+        println("Sua conta é positiva!")
+    } else if(saldo == 0.0) {
+        println("Sua conta é neutra.")
+    } else {
+        println("Sua conta é negativa.")
+    }
 
 }
+
 
